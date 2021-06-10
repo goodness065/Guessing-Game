925bd91615a4907286239aa0dc86f2e7205507ee
goodness065 <goodnesskelechi2017@yahoo.com>
2021-06-10 16:25:43 +0100

    The guessing game

---
+++ main.js
@@ -0,0 +1,81 @@
+let rectangle = document.getElementById('rectangle')
+let triangle = document.getElementById('triangle')
+let pentagon = document.getElementById('pentagon')
+let square = document.getElementById('square')
+let circle = document.getElementById('circle')
+let hexagon = document.getElementById('hexagon')
+let correct = document.getElementById("correct")
+let wrong = document.getElementById("wrong")
+
+// computerCorrect = style.display = "block"
+// computerWrong = style.display = "none"
+// userWrong = style.display = "block"
+// userCorrect = style.display = "none"
+
+let userGuessed = null;
+
+let computerGuess = 0;
+
+let userGuessedId = null;
+
+let borderChange = (name, index) => {
+
+  document.querySelectorAll(".sub").forEach((click) => click.style.border = "1px solid Transparent");
+
+  document.getElementById(name).style.border = "1px solid blue";
+
+  userGuessed = index
+
+  // document.getElementById("mapping").innerText = userGuessed
+  // return userGuessed
+  // document.getElementById("user-guessed").innerText = document.getElementById("o-" + (Math.floor(Math.random() * 6))).innerText
+}
+
+
+let guessShape = () => {
+  computerGuess = Math.floor(Math.random() * 7);
+  let correctVal = correct.textContent
+  let wrongVal = wrong.textContent
+  let userCorrect = document.querySelector('.result-sub1-img1')
+  let userWrong = document.querySelector('.result-sub1-img2')
+  let computerWrong = document.querySelector('result-sub2-img1')
+  let computerCorrect = document.querySelector('.result-sub2-img2')
+  let happy = document.querySelector(".happy")
+  let sad = document.querySelector(".sad")
+  alert(document.getElementById("o-" + computerGuess).innerText)
+
+  if (computerGuess == userGuessed) {
+    correct.textContent = Number(correctVal) + 1
+    happy.style.display = "block"
+    sad.style.display = "none"
+    // computerCorrect.style.display = "none"
+
+    computerWrong.style.display = "block"
+    // // userWrong.style.display = "none"
+    userCorrect.style.display = "block"
+  } else {
+    wrong.textContent = Number(wrongVal) + 1
+    sad.style.display = "block"
+    happy.style.display = "none"
+    computerCorrect.style.display = "block"
+    // computerWrong.style.display = "none"
+    userWrong.style.display = "block"
+    // userCorrect.style.display = "none"
+  }
+  //  alert(document.getElementById(`o-${computerGuess}`).innerText)
+  
+  document.querySelectorAll(".sub").forEach((click) => click.style.border = "1px solid Transparent");
+
+//   sad.style.display = "none"
+// happy.style.display = "none"
+
+}
+
+
+// }
+//When button is clicked, the guessSShape functioni is triggered
+let button = document.querySelector(".play-footer")
+button.addEventListener('click', () => guessShape())
+
+
+
